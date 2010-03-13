9e41608d85d7c2149ab6df5a8b98334eac343cb6
kabob <kabob@smokey>
2010-03-13 12:33:38 -0600

    - update development environment for use on gnash

--- config/environments/local.rb
+++ config/environments/local.rb
@@ -15,6 +15,3 @@ config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
-
-# relative url directive for passenger
-config.action_controller.relative_url_root = "/volunteer_index"
